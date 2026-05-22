446598d41f8729c22641a93fb6003a9ad038d3b0
AadeshRaut-06 <aadesh_raut@Aadyy.localdomain>
2026-05-22 12:34:57 +0000

    pom updated

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,21 @@
+import HeroSection from "../components/HeroSection";
+import BookingForm from "../components/BookingForm";
+import BookingTable from "../components/BookingTable";
+
+import "../styles/navbar.css";
+import "../styles/table.css";
+
 function AppRoutes() {
   return (
     <div>
-      Routes
+      <HeroSection />
+
+      <div className="container">
+        <BookingForm />
+        <BookingTable />
+      </div>
     </div>
-  )
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
